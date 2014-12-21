a55d7e6796366a8016d373e95962c602511fb88b
Alex Crichton <alex@alexcrichton.com>
2014-12-20 23:34:47 -0800

    Remove some no longer necessary tags

--- vendor/rust-lang/glob/src/lib.rs
+++ vendor/rust-lang/glob/src/lib.rs
@@ -20,10 +20,6 @@
 //! is implemented entirely in Rust rather than deferring to the libc
 //! `glob`/`fnmatch` functions.
 
-#![crate_name = "glob"]
-#![experimental]
-#![crate_type = "rlib"]
-#![crate_type = "dylib"]
 #![doc(html_logo_url = "http://www.rust-lang.org/logos/rust-logo-128x128-blk-v2.png",
        html_favicon_url = "http://www.rust-lang.org/favicon.ico")]
 
