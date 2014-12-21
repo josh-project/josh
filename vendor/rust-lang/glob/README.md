4267a86facf1e078991420a0876b5ca9b3d126cc
Alex Crichton <alex@alexcrichton.com>
2014-12-20 23:35:32 -0800

    Upload docs to travis CI

--- vendor/rust-lang/glob/README.md
+++ vendor/rust-lang/glob/README.md
@@ -5,6 +5,8 @@ Support for matching file paths against Unix shell style patterns.
 
 [![Build Status](https://travis-ci.org/rust-lang/glob.svg?branch=master)](https://travis-ci.org/rust-lang/glob)
 
+[Documentation](http://doc.rust-lang.org/glob)
+
 ## Usage
 
 To use `glob`, add this to your `Cargo.toml`:
--- vendor/rust-lang/glob/src/lib.rs
+++ vendor/rust-lang/glob/src/lib.rs
@@ -21,7 +21,8 @@
 //! `glob`/`fnmatch` functions.
 
 #![doc(html_logo_url = "http://www.rust-lang.org/logos/rust-logo-128x128-blk-v2.png",
-       html_favicon_url = "http://www.rust-lang.org/favicon.ico")]
+       html_favicon_url = "http://www.rust-lang.org/favicon.ico",
+       html_root_url = "http://doc.rust-lang.org/glob/")]
 
 use std::cell::Cell;
 use std::{cmp, os, path};
