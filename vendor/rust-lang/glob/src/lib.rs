bc73ba0c9b6e014de2039b783ae859e3c7494a6b
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2015-01-04 22:32:45 +0900

    Update rustc 0.13.0-nightly (c6c786671 2015-01-04 00:50:59 +0000)

--- vendor/rust-lang/glob/src/lib.rs
+++ vendor/rust-lang/glob/src/lib.rs
@@ -20,6 +20,7 @@
 //! is implemented entirely in Rust rather than deferring to the libc
 //! `glob`/`fnmatch` functions.
 
+#![feature(associated_types)]
 #![doc(html_logo_url = "http://www.rust-lang.org/logos/rust-logo-128x128-blk-v2.png",
        html_favicon_url = "http://www.rust-lang.org/favicon.ico",
        html_root_url = "http://doc.rust-lang.org/glob/")]
@@ -130,7 +131,8 @@ pub fn glob_with(pattern: &str, options: &MatchOptions) -> Paths {
     }
 }
 
-impl Iterator<Path> for Paths {
+impl Iterator for Paths {
+    type Item = Path;
 
     fn next(&mut self) -> Option<Path> {
         loop {
