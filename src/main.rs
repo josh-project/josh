6b3c5213d08169d16e2bda69b9a53e5193ae561a
Oliver Mueller <oliver.mueller@esrlabs.com>
2016-06-10 16:53:53 +0200

    warnig free

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@ extern crate clap;
 
 use git2::*;
 use std::env;
-use std::io::Write;
 use std::path::Path;
 use std::path::PathBuf;
 use std::process::Command;
@@ -17,7 +16,6 @@ const TMP_REPO_DIR:    &'static str = "/home/christian/gerrit_testsite/tmp_autom
 
 fn module_review_upload(project: &str, newrev: &str) {
   let project_path = Path::new(project);
-  let subdir = project_path.components().nth(0).unwrap();
   let module_name = project_path.components().last().unwrap();
 
   let tmp_repo = Repository::init_bare(TMP_REPO_DIR).unwrap();
@@ -30,13 +28,12 @@ fn module_review_upload(project: &str, newrev: &str) {
   let s = module_name.as_ref().to_str().unwrap();
   let oldrev = format!(
     "{}",tmp_repo.revparse_single(&format!("remotes/modules/{}/master",s)).unwrap().id()
-  );
+    );
 
   {
     let old = tmp_repo.revparse_single(&oldrev).unwrap().id();
     let new = tmp_repo.revparse_single(&newrev).unwrap().id();
 
-    let module_commit_obj = tmp_repo.revparse_single(&newrev).unwrap();
     if !tmp_repo.graph_descendant_of(new,old).unwrap() {
       println!(".");
       println!("===========================================================");
@@ -49,13 +46,13 @@ fn module_review_upload(project: &str, newrev: &str) {
   let walk = {
     let mut walk = tmp_repo.revwalk().unwrap();
     walk.set_sorting( Sort::from_bits(5).unwrap());
-    walk.push_range(&format!("{}..{}", oldrev, newrev));
+    let _ = walk.push_range(&format!("{}..{}", oldrev, newrev));
     walk
   };
 
 
-    println!("===== project path: {}", project);
-    println!("===== Apply commits from {} to {}", oldrev, newrev);
+  println!("===== project path: {}", project);
+  println!("===== Apply commits from {} to {}", oldrev, newrev);
 
   for rev in walk {
     let newrev = format!("{}",rev.unwrap());
@@ -85,14 +82,13 @@ fn module_review_upload(project: &str, newrev: &str) {
     &tmp_repo.find_commit(parent_commit_oid).unwrap(),
     CENTRAL_NAME,
     "refs/for/master"
-  );
+    );
   println!("{}", x);
   println!("==== The review upload may have worked, even if it says error below. Look UP! ====");
 }
 
 fn central_submit(newrev: &str) {
   println!("central_submit");
-  let mpath = Path::new("modules");
 
   let module_names = get_module_names(newrev);
   let tmp_repo = setup_tmp_repo(&module_names);
@@ -101,15 +97,14 @@ fn central_submit(newrev: &str) {
   let central_commit_obj = tmp_repo.revparse_single(newrev).unwrap();
   let central_commit = central_commit_obj.as_commit().unwrap();
   let central_tree = central_commit.tree().unwrap();
-  let modules = tmp_repo.find_tree(central_tree.get_path(mpath).unwrap().id());
-  tmp_repo.branch(CENTRAL_NAME,central_commit,true);
+  let _ = tmp_repo.branch(CENTRAL_NAME,central_commit,true);
 
   for module_name in module_names {
     let module_master_commit_obj =
       tmp_repo.revparse_single(&format!("remotes/modules/{}/master",module_name)).unwrap();
-    let module_master_commit = 
+    let module_master_commit =
       module_master_commit_obj.as_commit().unwrap();
-    tmp_repo.branch(&format!("modules/{}",module_name),module_master_commit,true);
+    let _ = tmp_repo.branch(&format!("modules/{}",module_name),module_master_commit,true);
 
     let parents = vec!(module_master_commit);
     let old_tree_oid = module_master_commit.tree().unwrap().id();
@@ -119,7 +114,7 @@ fn central_submit(newrev: &str) {
     let new_tree_oid = central_tree.get_path(&module_path).unwrap().id();
 
     if new_tree_oid != old_tree_oid {
-      
+
       let new_tree = tmp_repo.find_tree(new_tree_oid).unwrap();
 
       let module_commit = make_commit(&tmp_repo, &new_tree, central_commit, &parents);
@@ -128,7 +123,7 @@ fn central_submit(newrev: &str) {
         &tmp_repo.find_commit(module_commit.unwrap()).unwrap(),
         &format!("bsw/modules/{}",module_name),
         "master"
-      );
+        );
       println!("{}", x);
     }
   }
@@ -137,15 +132,15 @@ fn central_submit(newrev: &str) {
 fn transfer_to_tmp(rev: &str) {
   Command::new("git")
     .arg("branch").arg("-f").arg("tmp").arg(rev)
-  .output().expect("failed to call git");
+    .output().expect("failed to call git");
 
   Command::new("git")
     .arg("push").arg("--force").arg(TMP_REPO_DIR).arg("tmp")
-  .output().expect("failed to call git");
+    .output().expect("failed to call git");
 
   Command::new("git")
     .arg("branch").arg("-D").arg("tmp")
-  .output().expect("failed to call git");
+    .output().expect("failed to call git");
 }
 
 fn in_tmp_repo(cmd: &str) -> String {
@@ -160,32 +155,32 @@ fn setup_tmp_repo(modules: &Vec<String>) -> Repository {
   let repo = Repository::init_bare(TMP_REPO_DIR).unwrap();
 
   if !repo.find_remote("central_repo").is_ok() {
-    repo.remote("central_repo", 
-      &format!("ssh://{}@gerrit-test-git/{}.git",AUTOMATION_USER,CENTRAL_NAME)
-    );
+    let _ = repo.remote("central_repo",
+                        &format!("ssh://{}@gerrit-test-git/{}.git",AUTOMATION_USER,CENTRAL_NAME)
+                       );
   }
 
   for module in modules.iter() {
     let output = Command::new("ssh")
       .arg("-p").arg("29418")
       .arg("gerrit-test-git")
-        .arg("gerrit")
-        .arg("create-project")
-          .arg(format!("bsw/modules/{}",module))
-          .arg("--empty-commit")
+      .arg("gerrit")
+      .arg("create-project")
+      .arg(format!("bsw/modules/{}",module))
+      .arg("--empty-commit")
       .output()
       .expect("failed to create project");
 
     println!("create-project: {}", String::from_utf8_lossy(&output.stderr));
 
     let remote_url = format!("ssh://{}@gerrit-test-git:29418/bsw/modules/{}.git",
-      AUTOMATION_USER,
-      module
-    );
+                             AUTOMATION_USER,
+                             module
+                            );
 
     let remote_name = format!("modules/{}",module);
     if !repo.find_remote(&remote_name).is_ok() {
-        repo.remote(&remote_name, &remote_url);
+      let _ = repo.remote(&remote_name, &remote_url);
     }
   }
 
@@ -219,7 +214,7 @@ fn get_module_names(rev: &str) -> Vec<String> {
   let tree = commit.tree().unwrap();
 
   let modules_o = tree.get_path(&Path::new("modules")).unwrap()
-                      .to_object(&central_repo).unwrap();
+    .to_object(&central_repo).unwrap();
   let modules = modules_o.as_tree().unwrap();
 
   let mut names = Vec::<String>::new();
@@ -230,19 +225,19 @@ fn get_module_names(rev: &str) -> Vec<String> {
 }
 
 fn push_from_tmp(tmp_repo: &Repository, commit: &Commit,repo: &str ,to: &str) -> String {
-  tmp_repo.set_head_detached(commit.id());
-  return in_tmp_repo(
+  let _ = tmp_repo.set_head_detached(commit.id());
+  in_tmp_repo(
     &format!("push ssh://{}@gerrit-test-git:29418/{}.git HEAD:{}",
-      AUTOMATION_USER,
-      repo,
-      to
+             AUTOMATION_USER,
+             repo,
+             to
+            )
     )
-  )
 }
 
 fn make_commit(repo: &Repository, tree: &Tree, base: &Commit, parents: &[&Commit]) -> Option<Oid> {
   if parents.len() != 0 {
-    repo.set_head_detached(parents[0].id());
+    let _ = repo.set_head_detached(parents[0].id());
   }
   return repo.commit(
     Some("HEAD"),
@@ -251,42 +246,41 @@ fn make_commit(repo: &Repository, tree: &Tree, base: &Commit, parents: &[&Commit
     &base.message().unwrap_or("no message"),
     tree,
     parents
-  ).ok();
+    ).ok();
 }
 
 fn main() { exit(main_ret()); } fn main_ret() -> i32 {
 
   let args = clap::App::new("centralgithook")
-      .arg(clap::Arg::with_name("oldrev").long("oldrev").takes_value(true))
-      .arg(clap::Arg::with_name("newrev").long("newrev").takes_value(true))
-      .arg(clap::Arg::with_name("project").long("project").takes_value(true))
-      .arg(clap::Arg::with_name("refname").long("refname").takes_value(true))
-      .arg(clap::Arg::with_name("uploader").long("uploader").takes_value(true))
-      .arg(clap::Arg::with_name("commit").long("commit").takes_value(true))
-      .arg(clap::Arg::with_name("change").long("change").takes_value(true))
-      .arg(clap::Arg::with_name("change-url").long("change-url").takes_value(true))
-      .arg(clap::Arg::with_name("change-owner").long("change-owner").takes_value(true))
-      .arg(clap::Arg::with_name("branch").long("branch").takes_value(true))
-      .arg(clap::Arg::with_name("submitter").long("submitter").takes_value(true))
-      .arg(clap::Arg::with_name("topic").long("topic").takes_value(true))
-      .get_matches();
+    .arg(clap::Arg::with_name("oldrev").long("oldrev").takes_value(true))
+    .arg(clap::Arg::with_name("newrev").long("newrev").takes_value(true))
+    .arg(clap::Arg::with_name("project").long("project").takes_value(true))
+    .arg(clap::Arg::with_name("refname").long("refname").takes_value(true))
+    .arg(clap::Arg::with_name("uploader").long("uploader").takes_value(true))
+    .arg(clap::Arg::with_name("commit").long("commit").takes_value(true))
+    .arg(clap::Arg::with_name("change").long("change").takes_value(true))
+    .arg(clap::Arg::with_name("change-url").long("change-url").takes_value(true))
+    .arg(clap::Arg::with_name("change-owner").long("change-owner").takes_value(true))
+    .arg(clap::Arg::with_name("branch").long("branch").takes_value(true))
+    .arg(clap::Arg::with_name("submitter").long("submitter").takes_value(true))
+    .arg(clap::Arg::with_name("topic").long("topic").takes_value(true))
+    .get_matches();
 
-  let newrev = args.value_of("newrev").unwrap_or(""); 
-  let project = args.value_of("project").unwrap_or(""); 
-  let refname = args.value_of("refname").unwrap_or(""); 
-  let uploader = args.value_of("uploader").unwrap_or(""); 
-  let commit = args.value_of("commit").unwrap_or(""); 
+  let newrev = args.value_of("newrev").unwrap_or("");
+  let project = args.value_of("project").unwrap_or("");
+  let refname = args.value_of("refname").unwrap_or("");
+  let commit = args.value_of("commit").unwrap_or("");
 
 
   if let Some(hook) = env::args().nth(0) {
     let is_review = refname == "refs/for/master";
-    let is_automation = uploader.contains("Automation");
     let is_module = project != CENTRAL_NAME;
     let is_update = hook.ends_with("ref-update");
     let is_submit = hook.ends_with("change-merged");
 
     // // TODO
-    // if !is_review && !is_automation {
+    // let uploader = args.value_of("uploader").unwrap_or("");
+    // if !is_review && !uploader.contains("Automation") {
     //   println!("only push to refs/for/master");
     //   return 1;
     // }
