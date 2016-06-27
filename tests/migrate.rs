6a9c9dade705d98fd1da2f9229f2539802d6ecab
Christian Schilling <christian.schilling@esrlabs.com>
2016-06-27 16:34:03 +0200

    split test into multiple test cases

--- tests/migrate.rs
+++ tests/migrate.rs
@@ -16,14 +16,13 @@ struct TestSetup<'a>
     central: helpers::TestRepo,
     scratch: migrate::Scratch<'a>,
     shell: migrate::Shell,
-    head: String,
 }
 
 impl<'a> TestSetup<'a>
 {
     fn new(host: &'a RepoHost) -> Self
     {
-        env_logger::init().unwrap();
+        env_logger::init().ok();
         let td = TempDir::new("cgh_test").expect("folder cgh_test should be created");
         let central = helpers::TestRepo::new(&td.path().join("central"));
         let scratch = migrate::Scratch::new(&td.path().join("scratch"), host);
@@ -32,16 +31,11 @@ impl<'a> TestSetup<'a>
         host.create_project("central");
         central.shell.command(&format!("git remote add origin {}", &host.remote_url("central")));
 
-        central.add_file("modules/module_a/initial_a");
-        central.add_file("modules/module_b/initial_b");
-        let head = central.commit("initial");
-
         return TestSetup {
             td: td,
             central: central,
             scratch: scratch,
             shell: shell,
-            head: head,
         };
     }
 }
@@ -50,7 +44,11 @@ impl<'a> TestSetup<'a>
 fn test_initial_import()
 {
     let host = helpers::TestHost::new();
-    let TestSetup { td, central, scratch, shell, head } = TestSetup::new(&host);
+    let TestSetup { td, central, scratch, shell }  = TestSetup::new(&host);
+
+    central.add_file("modules/module_a/initial_a");
+    central.add_file("modules/module_b/initial_b");
+    let head = central.commit("initial");
 
     central.shell.command("git push origin master");
     migrate::central_submit(&scratch, scratch.transfer(&head, &host.repo_dir("central")))
@@ -58,26 +56,61 @@ fn test_initial_import()
 
     shell.command(&format!("git clone {}", &host.remote_url("modules/module_a")));
     shell.command(&format!("git clone {}", &host.remote_url("modules/module_b")));
+
     let module_a = helpers::TestRepo::new(&td.path().join("module_a"));
     let module_b = helpers::TestRepo::new(&td.path().join("module_b"));
 
     assert!(module_a.has_file("initial_a"));
     assert!(module_b.has_file("initial_b"));
+}
 
-    central.add_file("modules/module_a/added_a");
-    central.add_file("modules/module_c/added_c");
-    let head = central.commit("add");
+#[test]
+fn test_change_and_add_modules()
+{
+    let host = helpers::TestHost::new();
+    let TestSetup { td, central, scratch, shell }  = TestSetup::new(&host);
+
+    central.add_file("modules/module_a/initial_a");
+    let head = central.commit("initial");
 
     central.shell.command("git push origin master");
     migrate::central_submit(&scratch, scratch.transfer(&head, &host.repo_dir("central")))
         .expect("call central_submit");
 
-    module_a.shell.command("git pull");
+    central.add_file("modules/module_a/added_a");
+    central.add_file("modules/module_c/added_c");
+    let head = central.commit("add_a_and_c");
+
+    central.shell.command("git push origin master");
+    migrate::central_submit(&scratch, scratch.transfer(&head, &host.repo_dir("central")))
+        .expect("call central_submit");
+
+    shell.command(&format!("git clone {}", &host.remote_url("modules/module_a")));
     shell.command(&format!("git clone {}", &host.remote_url("modules/module_c")));
+
+    let module_a = helpers::TestRepo::new(&td.path().join("module_a"));
     let module_c = helpers::TestRepo::new(&td.path().join("module_c"));
 
     assert!(module_a.has_file("added_a"));
     assert!(module_c.has_file("added_c"));
+}
+
+#[test]
+fn test_module_review_upload()
+{
+    let host = helpers::TestHost::new();
+    let TestSetup { td, central, scratch, shell }  = TestSetup::new(&host);
+
+    central.add_file("modules/module_a/initial_a");
+    let head = central.commit("initial");
+
+    central.shell.command("git push origin master");
+    migrate::central_submit(&scratch, scratch.transfer(&head, &host.repo_dir("central")))
+        .expect("call central_submit");
+
+    shell.command(&format!("git clone {}", &host.remote_url("modules/module_a")));
+    let module_a = helpers::TestRepo::new(&td.path().join("module_a"));
+    module_a.shell.command("git pull");
 
     module_a.add_file("added/in_mod_a");
     let head = module_a.commit("module_a_commit");
