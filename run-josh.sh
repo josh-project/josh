d3762bfa476c5fafcc3363ba4089e3dd613cd847
Arthur Braga Alfredo <arthurbalfredo@gmail.com>
2019-07-25 10:04:06 +0200

    Update run-josh.sh
    Use environment variable JOSH_REMOTE instead of hard codeded url.

--- run-josh.sh
+++ run-josh.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 DATE=$(date)
 mkdir -p "/data/logs/${DATE}/"
-RUST_BACKTRACE=1 josh-proxy --local=/tmp/josh-scratch/ --remote=https://gerrit.int.esrlabs.com --trace "/data/logs/${DATE}/" | tee "/data/logs/${DATE}/out.log"
+RUST_BACKTRACE=1 josh-proxy --local=/tmp/josh-scratch/ --remote="${JOSH_REMOTE}" --trace "/data/logs/${DATE}/" | tee "/data/logs/${DATE}/out.log"
