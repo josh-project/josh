1dffc319c82bbc96ff0be5a2aa43764814c5d5d9
louis-marie.givel <louis-marie.givel@esrlabs.com>
2020-09-30 09:44:37 +0200

    update test server2

--- josh-test-server2/src/bin/josh-test-server2.rs
+++ josh-test-server2/src/bin/josh-test-server2.rs
@@ -1,55 +1,176 @@
-use hyper::header::AUTHORIZATION;
+use base64;
+use futures::future;
+use futures::FutureExt;
+use futures::TryStreamExt;
 use hyper::service::{make_service_fn, service_fn};
-use hyper::{Body, Error, Request, Response, Server};
+use hyper::{Request, Response, Server};
 use std::env;
-use std::net::SocketAddr;
-use std::sync::atomic::{AtomicUsize, Ordering};
-use std::sync::{Arc, RwLock};
+use std::net;
+use std::path::Path;
+use std::path::PathBuf;
+use std::process::Stdio;
+use std::sync::Arc;
+use tokio::process::Command;
+use tokio::stream::StreamExt;
+use tokio::io::BufReader;
+use tokio::io::AsyncBufReadExt;
+use tokio::io::AsyncReadExt;
+use std::str::FromStr;
 
-/* fn auth_response( */
-/*     req: &Request<Body>, */
-/*     username: &str, */
-/*     password: &str, */
-/* ) -> Option<Response<Body>> { */
-/*     let (rusername, rpassword) = match req.headers().get() { */
-/*         Some(&Authorization(Basic { */
-/*             ref username, */
-/*             ref password, */
-/*         })) => ( */
-/*             username.to_owned(), */
-/*             password.to_owned().unwrap_or_else(|| "".to_owned()), */
-/*         ), */
-/*         _ => { */
-/*             println!("ServeTestGit: no credentials in request"); */
-/*             let mut response = */
-/*                 Response::new().with_status(hyper::StatusCode::Unauthorized); */
-/*             response.headers_mut().set_raw( */
-/*                 "WWW-Authenticate", */
-/*                 "Basic realm=\"User Visible Realm\"", */
-/*             ); */
-/*             return Some(response); */
-/*         } */
-/*     }; */
+#[macro_export]
+macro_rules! some_or {
+    ($e:expr, $b:block) => {
+        if let Some(x) = $e {
+            x
+        } else {
+            $b
+        }
+    };
+}
 
-/*     if rusername != "admin" && (rusername != username || rpassword != password) */
-/*     { */
-/*         println!("ServeTestGit: wrong user/pass"); */
-/*         println!("user: {:?} - {:?}", rusername, username); */
-/*         println!("pass: {:?} - {:?}", rpassword, password); */
-/*         let mut response = */
-/*             Response::new().with_status(hyper::StatusCode::Unauthorized); */
-/*         response */
-/*             .headers_mut() */
-/*             .set_raw("WWW-Authenticate", "Basic realm=\"User Visible Realm\""); */
-/*         return Some(response); */
-/*     } */
+#[macro_export]
+macro_rules! ok_or {
+    ($e:expr, $b:block) => {
+        if let Ok(x) = $e {
+            x
+        } else {
+            $b
+        }
+    };
+}
 
-/*     println!("CREDENTIALS OK {:?} {:?}", &rusername, &rpassword); */
-/*     return None; */
-/* } */
+pub struct ServeTestGit {
+    repo_path: PathBuf,
+    username: String,
+    password: String,
+}
 
-struct MyService {
-    num: usize,
+pub fn parse_auth(
+    req: &hyper::Request<hyper::Body>,
+) -> Option<(String, String)> {
+    let line = some_or!(
+        req.headers()
+            .get("authorization")
+            .and_then(|h| Some(h.as_bytes())),
+        {
+            return None;
+        }
+    );
+    let u = ok_or!(String::from_utf8(line[6..].to_vec()), {
+        return None;
+    });
+    let decoded = ok_or!(base64::decode(&u), {
+        return None;
+    });
+    let s = ok_or!(String::from_utf8(decoded), {
+        return None;
+    });
+    if let [username, password] =
+        s.as_str().split(':').collect::<Vec<_>>().as_slice()
+    {
+        return Some((username.to_string(), password.to_string()));
+    }
+    return None;
+}
+
+fn auth_response(
+    req: &Request<hyper::Body>,
+    username: &str,
+    password: &str,
+) -> Option<Response<hyper::Body>> {
+    let (rusername, rpassword) = match parse_auth(req) {
+        Some(x) => x,
+        None => {
+            println!("ServeTestGit: no credentials in request");
+            let builder = Response::builder()
+                .header("WWW-Authenticate", "")
+                .header("Basic realm", "User Visible Realm")
+                .status(hyper::StatusCode::UNAUTHORIZED);
+            return Some(
+                builder
+                    .body(hyper::Body::empty())
+                    .unwrap_or(Response::default()),
+            );
+        }
+    };
+
+    if rusername != "admin" && (rusername != username || rpassword != password)
+    {
+        println!("ServeTestGit: wrong user/pass");
+        println!("user: {:?} - {:?}", rusername, username);
+        println!("pass: {:?} - {:?}", rpassword, password);
+        let builder = Response::builder()
+            .header("WWW-Authenticate", "")
+            .header("Basic realm", "User Visible Realm")
+            .status(hyper::StatusCode::UNAUTHORIZED);
+        return Some(
+            builder
+                .body(hyper::Body::empty())
+                .unwrap_or(Response::default()),
+        );
+    }
+
+    println!("CREDENTIALS OK {:?} {:?}", &rusername, &rpassword);
+    return None;
+}
+
+async fn call(
+    serv: Arc<ServeTestGit>,
+    req: Request<hyper::Body>,
+) -> Response<hyper::Body> {
+    println!("call");
+
+    if let Some(response) = auth_response(&req, &serv.username, &serv.password)
+    {
+        return response;
+    }
+
+    let path = &serv.repo_path;
+
+    println!("ServeTestGit CALLING git-http backend");
+    let mut cmd = Command::new("git");
+    cmd.arg("http-backend");
+    cmd.current_dir(&path);
+    cmd.env("GIT_PROJECT_ROOT", &path);
+    /* cmd.env("PATH_TRANSLATED", "/"); */
+    cmd.env("GIT_DIR", &path);
+    cmd.env("GIT_HTTP_EXPORT_ALL", "");
+    cmd.env("PATH_INFO", req.uri().path());
+
+    do_cgi(req, cmd).await
+}
+
+async fn run_test_server(
+    addr: net::SocketAddr,
+    repo_path: &Path,
+    username: &str,
+    password: &str,
+) {
+    let serve_test_git = Arc::new(ServeTestGit {
+        repo_path: repo_path.to_owned(),
+        username: username.to_owned(),
+        password: password.to_owned(),
+    });
+
+    let make_service = make_service_fn(move |_| {
+        let serve_test_git = serve_test_git.clone();
+
+        let service = service_fn(move |_req| {
+            let serve_test_git = serve_test_git.clone();
+
+            call(serve_test_git, _req).map(Ok::<_, hyper::http::Error>)
+        });
+
+        future::ok::<_, hyper::http::Error>(service)
+    });
+
+    let server = Server::bind(&addr).serve(make_service);
+
+    println!("Now listening on {}", addr);
+
+    if let Err(e) = server.await {
+        eprintln!("server error: {}", e);
+    }
 }
 
 #[tokio::main]
@@ -61,6 +182,11 @@ async fn main() {
         }
         args
     };
+
+    println!("RUN HTTP SERVER {:?}", args);
+
+    println!("args: {:?}", args);
+
     let app = clap::App::new("josh-test-server")
         .arg(
             clap::Arg::with_name("local")
@@ -82,31 +208,136 @@ async fn main() {
     let args = app.get_matches_from(args);
 
     let port = args.value_of("port").unwrap_or("8000").to_owned();
+
     let addr = format!("0.0.0.0:{}", port).parse().unwrap();
+    run_test_server(
+        addr,
+        &PathBuf::from(
+            args.value_of("local").expect("missing local directory"),
+        ),
+        args.value_of("username").expect("missing username"),
+        args.value_of("password").expect("missing password"),
+    ).await;
 
-    let myservice = Arc::new(MyService { num: 0 });
+    ()
+}
 
-    let make_service = make_service_fn(move |_| {
-        let myservice = myservice.clone();
+async fn do_cgi(
+    req: Request<hyper::Body>,
+    cmd: Command,
+) -> hyper::http::Response<hyper::Body> {
+    let mut cmd = cmd;
+    cmd.stdout(Stdio::piped());
+    cmd.stderr(Stdio::piped());
+    cmd.stdin(Stdio::piped());
+    cmd.env("SERVER_SOFTWARE", "hyper")
+        .env("SERVER_NAME", "localhost") // TODO
+        .env("GATEWAY_INTERFACE", "CGI/1.1")
+        .env("SERVER_PROTOCOL", "HTTP/1.1") // TODO
+        .env("SERVER_PORT", "80") // TODO
+        .env("REQUEST_METHOD", format!("{}", req.method()))
+        .env("SCRIPT_NAME", "") // TODO
+        .env("QUERY_STRING", req.uri().query().unwrap_or(""))
+        .env("REMOTE_ADDR", "") // TODO
+        .env("AUTH_TYPE", "") // TODO
+        .env("REMOTE_USER", "") // TODO
+        .env(
+            "CONTENT_TYPE",
+            &format!(
+                "{:?}",
+                req.headers().get(hyper::header::CONTENT_TYPE).unwrap()
+            ),
+        )
+        .env(
+            "HTTP_CONTENT_ENCODING",
+            &format!(
+                "{:?}",
+                req.headers().get(hyper::header::CONTENT_ENCODING).unwrap()
+            ),
+        )
+        .env(
+            "CONTENT_LENGTH",
+            &format!(
+                "{:?}",
+                req.headers().get(hyper::header::CONTENT_LENGTH).unwrap()
+            ),
+        );
 
-        async move {
-            Ok::<_, Error>(service_fn(move |_req| {
-                let myservice = myservice.clone();
-                async move {
-                    Ok::<_, Error>(Response::new(Body::from(format!(
-                        "Request #{}",
-                        myservice.num
-                    ))))
-                }
-            }))
+    let mut child = cmd.spawn().expect("can't spawn CGI command");
+    let mut stdin = child.stdin.as_mut().expect("Failed to open stdin");
+    let mut stdout = child.stdout.as_mut().expect("Failed to open stdout");
+    let mut stderr = child.stderr.as_mut().expect("Failed to open stderr");
+
+    let req_body = req
+        .into_body()
+        .map(|result| {
+            result.map_err(|_error| {
+                std::io::Error::new(std::io::ErrorKind::Other, "Error!")
+            })
+        })
+        .into_async_read();
+
+    let mut req_body = to_tokio_async_read(req_body);
+
+    let res = tokio::try_join!(
+        tokio::io::copy(&mut req_body, &mut stdin),
+        build_response(&mut stdout, &mut stderr)
+    );
+
+    let (_, r2) = res.unwrap();
+
+    r2
+}
+
+fn to_tokio_async_read(
+    r: impl futures::io::AsyncRead,
+) -> impl tokio::io::AsyncRead {
+    tokio_util::compat::FuturesAsyncReadCompatExt::compat(r)
+}
+
+async fn build_response(
+    stdout: &mut &mut tokio::process::ChildStdout,
+    stderr: &mut &mut tokio::process::ChildStderr,
+) -> Result<Response<hyper::Body>, std::io::Error> {
+    let mut response = Response::builder();
+
+    let mut headers = vec![];
+    let mut stdout = BufReader::new(stdout);
+    let mut line = String::new();
+    while stdout.read_line(&mut line).await.unwrap_or(0) > 0 {
+        if line.is_empty() {
+            break;
         }
-    });
+        let l: Vec<&str> =
+            line.splitn(2, ": ").collect();
+        for x in &l {
+            headers.push(x.to_string());
+        }
+        if l[0] == "Status" {
+            response = response.status(hyper::StatusCode::from_u16(
+                u16::from_str(l[1].split(" ").next().unwrap()).unwrap(),
+            ).unwrap());
+        } else {
+            response = response
+                .header(l[0], l[1]);
+        }
+    }
 
-    // Then bind and serve...
-    let server = Server::bind(&addr).serve(make_service);
+    let mut data = vec![];
+    stdout
+        .read_to_end(&mut data).await.unwrap();
 
-    // And run forever...
-    if let Err(e) = server.await {
-        eprintln!("server error: {}", e);
+    let mut stderrdata = vec![];
+    stderr
+        .read_to_end(&mut stderrdata).await.unwrap();
+
+    convert_error_io_hyper(response.body(hyper::Body::from(data)))
+}
+
+fn convert_error_io_hyper<T>(res: Result<T, hyper::http::Error>) -> Result<T, std::io::Error>
+{
+    match res {
+        Ok(res) => Ok(res),
+        Err(_) => Err(std::io::Error::new(std::io::ErrorKind::Other, "Error!"))
     }
 }
