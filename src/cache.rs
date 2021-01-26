b1b072e3d7c230378430110fc6462bdb9f667285
Christian Schilling <christian.schilling@esrlabs.com>
2021-01-26 20:05:57 +0100

    No status printing for now

--- src/cache.rs
+++ src/cache.rs
@@ -88,10 +88,10 @@ impl Transaction {
         ))
     }
 
-    pub fn status(&self, msg: &str) {
-        let mut t2 = self.t2.borrow_mut();
-        write!(t2.out, "{}", msg).ok();
-        t2.out.flush().ok();
+    pub fn status(&self, _msg: &str) {
+        /* let mut t2 = self.t2.borrow_mut(); */
+        /* write!(t2.out, "{}", msg).ok(); */
+        /* t2.out.flush().ok(); */
     }
 
     pub fn new2(
