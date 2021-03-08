78240b8dc2185deecbe4f16f75c9b0f054cd94b8
Christian Schilling <christian.schilling@esrlabs.com>
2021-03-08 18:14:55 +0100

    Don't fetch gerrit changes by default

--- josh-proxy/src/bin/josh-proxy.rs
+++ josh-proxy/src/bin/josh-proxy.rs
@@ -71,9 +71,9 @@ async fn fetch_upstream(
 
     let refs_to_fetch = if headref != "" && !headref.starts_with("refs/heads/")
     {
-        vec!["refs/changes/*", "refs/heads/*", "refs/tags/*", headref]
+        vec!["refs/heads/*", "refs/tags/*", headref]
     } else {
-        vec!["refs/changes/*", "refs/heads/*", "refs/tags/*"]
+        vec!["refs/heads/*", "refs/tags/*"]
     };
 
     let refs_to_fetch: Vec<_> =
