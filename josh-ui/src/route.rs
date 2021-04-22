84dab5b0f5649e07bb02a1dc612857c98383bff4
Louis-Marie Givel <louis-marie.givel@esrlabs.com>
2021-04-22 14:56:54 +0200

    ui: replace filter dropdown with page

--- josh-ui/src/route.rs
+++ josh-ui/src/route.rs
@@ -4,12 +4,15 @@ use super::*;
 pub enum AppRoute {
     #[to = "/~/browse/{*}@{*}({*})/{*}"]
     Browse(String, String, String, String),
+    #[to = "/~/filter/{*}@{*}({*})"]
+    Filter(String, String, String),
 }
 
 impl AppRoute {
     pub fn with_path(&self, path: &str) -> Self {
         match self.clone() {
             Self::Browse(repo, rev, filter, _) => Self::Browse(repo, rev, filter, path.to_string()),
+            _ => self.clone(),
         }
     }
 
@@ -26,6 +29,14 @@ impl AppRoute {
                         .unwrap_or_default(),
                 )
             }
+            _ => self.clone(),
+        }
+    }
+
+    pub fn edit_filter(&self) -> Self {
+        match self.clone() {
+            Self::Browse(repo, rev, filter, _path) => Self::Filter(repo, rev, filter),
+            _ => self.clone(),
         }
     }
 
@@ -37,6 +48,7 @@ impl AppRoute {
                     .map(|x| x.to_string_lossy().to_string())
                     .unwrap_or_default()
             }
+            _ => "".to_string(),
         }
     }
 
@@ -58,6 +70,9 @@ impl AppRoute {
     pub fn with_filter(&self, filter: &str) -> Self {
         match self.clone() {
             Self::Browse(repo, rev, _, path) => Self::Browse(repo, rev, filter.to_string(), path),
+            Self::Filter(repo, rev, _) => {
+                Self::Browse(repo, rev, filter.to_string(), "".to_string())
+            }
         }
     }
 
@@ -66,30 +81,35 @@ impl AppRoute {
             Self::Browse(repo, _, filter, path) => {
                 Self::Browse(repo, rev.to_string(), filter, path)
             }
+            Self::Filter(repo, _, filter) => Self::Filter(repo, rev.to_string(), filter),
         }
     }
 
     pub fn repo(&self) -> String {
         match self.clone() {
             Self::Browse(repo, _, _, _) => repo,
+            Self::Filter(repo, _, _) => repo,
         }
     }
 
     pub fn rev(&self) -> String {
         match self.clone() {
             Self::Browse(_, rev, _, _) => rev,
+            Self::Filter(_, rev, _) => rev,
         }
     }
 
     pub fn filter(&self) -> String {
         match self.clone() {
             Self::Browse(_, _, filter, _) => filter,
+            Self::Filter(_, _, filter) => filter,
         }
     }
 
     pub fn path(&self) -> String {
         match self.clone() {
             Self::Browse(_, _, _, path) => path,
+            _ => "".to_string(),
         }
     }
 }
