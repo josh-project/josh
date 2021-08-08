91dde46480e4c2f7fb28dece9d690dfda9962209
Christian Schilling <initcrash@gmail.com>
2021-08-08 11:04:02 +0000

    Prepare gitpod support

--- run-tests.sh
+++ run-tests.sh
@@ -10,4 +10,6 @@ export GIT_COMMITTER_EMAIL=josh@example.com
 export GIT_COMMITTER_DATE="2005-04-07T22:13:13"
 export EMPTY_TREE="4b825dc642cb6eb9a060e54bf8d69288fbee4904"
 
+git config --global init.defaultBranch master
+
 python3 -m cram "$@"
