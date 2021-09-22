3a88744ed03e5a1a5506958e369ad60d940a8bdf
Nex Zhu <4370605+NexZhu@users.noreply.github.com>
2021-09-22 23:28:58 +0800

    chore: add `JOSH_ACL` option to `run-josh.sh`

--- run-josh.sh
+++ run-josh.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+[ -n "${JOSH_ACL}" ] && acl_arg="--acl ${JOSH_ACL}"
 cd /josh/
-RUST_BACKTRACE=1 josh-proxy --gc --local=/data/git/ --remote="${JOSH_REMOTE}" ${JOSH_EXTRA_OPTS}
+RUST_BACKTRACE=1 josh-proxy --gc --local=/data/git/ --remote="${JOSH_REMOTE}" ${acl_arg} ${JOSH_EXTRA_OPTS}
