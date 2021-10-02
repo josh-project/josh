12c00de6ed1264f9dde7ee9b170c9d7a6f64a4f1
Louis-Marie Givel <louis-marie.givel@esrlabs.com>
2021-10-02 09:34:12 +0200

    Pin nix pkg version and add rust toolchain in shell.nix

--- shell.nix
+++ shell.nix
@@ -4,16 +4,25 @@ let
   extra_deps = if stdenv.isDarwin then [
     darwin.apple_sdk.frameworks.Security
   ] else [];
-
+  pkgs = import ( fetchTarball {
+      name = "nixos-21.05";
+      url =  "https://github.com/NixOS/nixpkgs/archive/refs/tags/21.05.tar.gz";
+      # Hash obtained using `nix-prefetch-url --unpack <url>`
+      sha256 = "1ckzhh24mgz6jd1xhfgx0i9mijk6xjqxwsshnvq789xsavrmsc36";
+  }) {};
+  rust_channel = nixpkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain;
 in
-   mkShell {
+   pkgs.mkShell {
      buildInputs = [
-       git
-       tree
-       cargo
-       libiconv
-       openssl.dev
-       pkgconfig
-       python39Packages.cram
+       pkgs.git
+       pkgs.tree
+       pkgs.cargo
+       pkgs.rustc
+       pkgs.rustfmt
+       pkgs.libiconv
+       pkgs.openssl.dev
+       pkgs.pkgconfig
+       pkgs.python39Packages.cram
      ] ++ extra_deps;
+     RUST_BACKTRACE = 1;
    }
