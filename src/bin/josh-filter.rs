7098da4fe4c72e17b4f6c78f1837a29111c3f381
Louis-Marie Givel <louis-marie.givel@esrlabs.com>
2022-10-06 20:18:05 +0200

    Update depdendencies (#968)
    Especially Clap to version 4
    
    Change: clap-update

--- src/bin/josh-filter.rs
+++ src/bin/josh-filter.rs
@@ -8,176 +8,162 @@ use josh::JoshError;
 use std::fs::read_to_string;
 use std::io::Write;
 
-fn make_app() -> clap::Command<'static> {
+fn make_app() -> clap::Command {
     let app = clap::Command::new("josh-filter");
 
     #[cfg(feature = "search")]
-    let app = { app.arg(clap::Arg::new("search").long("search").takes_value(true)) };
+    let app = { app.arg(clap::Arg::new("search").long("search")) };
     let app = app
         .arg(
             clap::Arg::new("filter")
                 .help("Filter to apply")
-                .default_value(":/")
-                .takes_value(true),
+                .default_value(":/"),
         )
         .arg(
             clap::Arg::new("input")
                 .help("Ref to apply filter to")
-                .default_value("HEAD")
-                .takes_value(true),
+                .default_value("HEAD"),
         )
         .arg(
             clap::Arg::new("file")
                 .long("file")
-                .help("Read filter spec from file")
-                .takes_value(true),
+                .help("Read filter spec from file"),
         )
         .arg(
             clap::Arg::new("update")
                 .long("update")
                 .help("reference to update with the result")
-                .default_value("FILTERED_HEAD")
-                .takes_value(true),
+                .default_value("FILTERED_HEAD"),
         )
         .arg(
             clap::Arg::new("squash")
+                .action(clap::ArgAction::SetTrue)
                 .help("Only output one commit, without history")
                 .long("squash"),
         )
         .arg(
             clap::Arg::new("discover")
+                .action(clap::ArgAction::SetTrue)
                 .help("Populate the cache with probable filters")
                 .short('d'),
         )
         .arg(
             clap::Arg::new("trace")
+                .action(clap::ArgAction::SetTrue)
                 .help("Write a trace in chrome tracing format")
                 .short('t'),
         )
         .arg(
             clap::Arg::new("print-filter")
+                .action(clap::ArgAction::SetTrue)
                 .help("Pretty print the filter and exit")
                 .short('p'),
         )
         .arg(
             clap::Arg::new("cache-stats")
+                .action(clap::ArgAction::SetTrue)
                 .help("Show stats about cache content")
                 .short('s'),
         )
         .arg(
             clap::Arg::new("no-cache")
+                .action(clap::ArgAction::SetTrue)
                 .help("Don't load cache")
                 .short('n'),
         )
         .arg(
             clap::Arg::new("pack")
+                .action(clap::ArgAction::SetTrue)
                 .help("Write a packfile instead of loose objects")
                 .long("pack"),
         )
-        .arg(
-            clap::Arg::new("query")
-                .long("query")
-                .short('q')
-                .takes_value(true),
-        )
+        .arg(clap::Arg::new("query").long("query").short('q'))
         .arg(
             clap::Arg::new("graphql")
                 .long("graphql")
-                .short('g')
-                .takes_value(true),
+                .short('g'),
         )
         .arg(
             clap::Arg::new("max_comp")
                 .long("max_comp")
-                .short('m')
-                .takes_value(true),
+                .short('m'),
         )
         .arg(
-            clap::Arg::new("reverse").long("reverse").help(
+            clap::Arg::new("reverse").action(clap::ArgAction::SetTrue).long("reverse").help(
                 "reverse-apply the filter to the output reference to update the input referebce",
             ),
         )
         .arg(
             clap::Arg::new("check-permission")
+                .action(clap::ArgAction::SetTrue)
                 .long("check-permission")
                 .short('c'),
         )
-        .arg(clap::Arg::new("missing-permission").long("missing-permission"))
+        .arg(clap::Arg::new("missing-permission").long("missing-permission")
+                .action(clap::ArgAction::SetTrue))
         .arg(
             clap::Arg::new("whitelist")
                 .long("whitelist")
-                .short('w')
-                .takes_value(true),
+                .short('w'),
         )
         .arg(
             clap::Arg::new("blacklist")
                 .long("blacklist")
-                .short('b')
-                .takes_value(true),
+                .short('b'),
         )
-        .arg(clap::Arg::new("users").long("users").takes_value(true))
-        .arg(clap::Arg::new("groups").long("groups").takes_value(true))
-        .arg(
-            clap::Arg::new("user")
-                .long("user")
-                .short('u')
-                .takes_value(true),
-        )
-        .arg(
-            clap::Arg::new("repo")
-                .long("repo")
-                .short('r')
-                .takes_value(true),
-        )
-        .arg(clap::Arg::new("version").long("version").short('v'));
+        .arg(clap::Arg::new("users").long("users"))
+        .arg(clap::Arg::new("groups").long("groups"))
+        .arg(clap::Arg::new("user").long("user").short('u'))
+        .arg(clap::Arg::new("repo").long("repo").short('r'))
+        .arg(clap::Arg::new("version").action(clap::ArgAction::SetTrue).long("version").short('v'));
     return app;
 }
 
 fn run_filter(args: Vec<String>) -> josh::JoshResult<i32> {
     let args = make_app().get_matches_from(args);
 
-    if args.is_present("trace") {
+    if args.get_flag("trace") {
         rs_tracing::open_trace_file!(".").unwrap();
     }
 
-    if args.is_present("version") {
+    if args.get_flag("version") {
         println!("Version: {}", josh::VERSION);
         return Ok(0);
     }
-    let specstr = args.value_of("filter").unwrap();
+    let specstr = args.get_one::<String>("filter").unwrap();
     let specstr = args
-        .value_of("file")
+        .get_one::<String>("file")
         .and_then(|f| read_to_string(f).ok())
         .unwrap_or(specstr.to_string());
 
     let mut filterobj = josh::filter::parse(&specstr)?;
 
-    if args.is_present("squash") {
+    if args.get_flag("squash") {
         filterobj = josh::filter::chain(josh::filter::parse(":SQUASH")?, filterobj);
     }
 
-    if args.is_present("print-filter") {
-        let filterobj = if args.is_present("reverse") {
+    if args.get_flag("print-filter") {
+        let filterobj = if args.get_flag("reverse") {
             josh::filter::invert(filterobj)?
         } else {
             filterobj
         };
         println!(
             "{}",
-            josh::filter::pretty(filterobj, if args.is_present("file") { 0 } else { 4 })
+            josh::filter::pretty(filterobj, if args.contains_id("file") { 0 } else { 4 })
         );
         return Ok(0);
     }
 
     let repo = git2::Repository::open_from_env()?;
-    if !args.is_present("no-cache") {
+    if !args.get_flag("no-cache") {
         josh::cache::load(repo.path())?;
     }
     let transaction = josh::cache::Transaction::new(repo, None);
     let repo = transaction.repo();
 
     let odb = repo.odb()?;
-    let mp = if args.is_present("pack") {
+    let mp = if args.get_flag("pack") {
         let mempack = odb.add_new_mempack_backend(1000)?;
         Some(mempack)
     } else {
@@ -185,10 +171,10 @@ fn run_filter(args: Vec<String>) -> josh::JoshResult<i32> {
     };
 
     let finish = defer::defer(|| {
-        if args.is_present("trace") {
+        if args.get_flag("trace") {
             rs_tracing::close_trace_file!();
         }
-        if args.is_present("cache-stats") {
+        if args.get_flag("cache-stats") {
             josh::cache::print_stats();
         }
         if let Some(mempack) = mp {
@@ -202,9 +188,9 @@ fn run_filter(args: Vec<String>) -> josh::JoshResult<i32> {
         }
     });
 
-    let input_ref = args.value_of("input").unwrap();
+    let input_ref = args.get_one::<String>("input").unwrap();
 
-    if args.is_present("discover") {
+    if args.get_flag("discover") {
         let r = repo.revparse_single(input_ref)?;
         let hs = josh::housekeeping::find_all_workspaces_and_subdirectories(&r.peel_to_tree()?)?;
         for i in hs {
@@ -222,12 +208,12 @@ fn run_filter(args: Vec<String>) -> josh::JoshResult<i32> {
         }
     }
 
-    let update_target = args.value_of("update").unwrap();
+    let update_target = args.get_one::<String>("update").unwrap();
 
     let src = input_ref;
     let target = update_target;
 
-    let reverse = args.is_present("reverse");
+    let reverse = args.get_flag("reverse");
 
     let t = if reverse {
         "refs/JOSH_TMP".to_owned()
@@ -241,30 +227,30 @@ fn run_filter(args: Vec<String>) -> josh::JoshResult<i32> {
 
     let src = src_r.name().unwrap().to_string();
 
-    let check_permissions = args.is_present("check-permission");
+    let check_permissions = args.get_flag("check-permission");
     let mut permissions_filter = josh::filter::empty();
     if check_permissions {
         let whitelist;
         let blacklist;
-        if args.is_present("users")
-            && args.is_present("groups")
-            && args.is_present("user")
-            && args.is_present("repo")
+        if args.contains_id("users")
+            && args.contains_id("groups")
+            && args.contains_id("user")
+            && args.contains_id("repo")
         {
-            let users = args.value_of("users").unwrap();
-            let groups = args.value_of("groups").unwrap();
-            let user = args.value_of("user").unwrap();
-            let repo = args.value_of("repo").unwrap();
+            let users = args.get_one::<String>("users").unwrap();
+            let groups = args.get_one::<String>("groups").unwrap();
+            let user = args.get_one::<String>("user").unwrap();
+            let repo = args.get_one::<String>("repo").unwrap();
 
             let acl = josh::get_acl(users, groups, user, repo)?;
             whitelist = acl.0;
             blacklist = acl.1;
         } else {
-            whitelist = match args.value_of("whitelist") {
+            whitelist = match args.get_one::<String>("whitelist") {
                 Some(s) => josh::filter::parse(s)?,
                 _ => josh::filter::nop(),
             };
-            blacklist = match args.value_of("blacklist") {
+            blacklist = match args.get_one::<String>("blacklist") {
                 Some(s) => josh::filter::parse(s)?,
                 _ => josh::filter::empty(),
             };
@@ -272,7 +258,7 @@ fn run_filter(args: Vec<String>) -> josh::JoshResult<i32> {
         permissions_filter = josh::filter::make_permissions_filter(filterobj, whitelist, blacklist)
     }
 
-    let missing_permissions = args.is_present("missing-permission");
+    let missing_permissions = args.get_flag("missing-permission");
     if missing_permissions {
         filterobj = permissions_filter;
         permissions_filter = josh::filter::empty();
@@ -291,21 +277,21 @@ fn run_filter(args: Vec<String>) -> josh::JoshResult<i32> {
     )?;
     updated_refs[0].0 = t;
     josh::update_refs(&transaction, &mut updated_refs, "");
-    if args.value_of("update") != Some("FILTERED_HEAD")
+    if args.get_one::<String>("update").map(|v| v.as_str()) != Some("FILTERED_HEAD")
         && updated_refs.len() == 1
         && updated_refs[0].1 == old_oid
     {
         println!(
             "Warning: reference {} wasn't updated",
-            args.value_of("update").unwrap()
+            args.get_one::<String>("update").unwrap()
         );
     }
 
     #[cfg(feature = "search")]
-    if let Some(searchstring) = args.value_of("search") {
+    if let Some(searchstring) = args.get_one::<String>("search") {
         let ifilterobj = josh::filter::chain(filterobj, josh::filter::parse(":SQUASH:INDEX")?);
 
-        let max_complexity: usize = args.value_of("max_comp").unwrap_or("6").parse()?;
+        let max_complexity: usize = args.get_one::<String>("max_comp").unwrap_or("6").parse()?;
 
         josh::filter_ref(
             &transaction,
@@ -360,7 +346,7 @@ fn run_filter(args: Vec<String>) -> josh::JoshResult<i32> {
         }
     }
 
-    if let Some(gql_query) = args.value_of("graphql") {
+    if let Some(gql_query) = args.get_one::<String>("graphql") {
         let context = josh::graphql::context(transaction.try_clone()?, transaction.try_clone()?);
         *context.allow_refs.lock()? = true;
         let (res, _errors) = juniper::execute_sync(
@@ -377,7 +363,7 @@ fn run_filter(args: Vec<String>) -> josh::JoshResult<i32> {
 
     std::mem::drop(finish);
 
-    if let Some(query) = args.value_of("query") {
+    if let Some(query) = args.get_one::<String>("query") {
         let repo = git2::Repository::open_from_env()?;
         let transaction = josh::cache::Transaction::new(repo, None);
         let commit_id = transaction.repo().refname_to_id(&update_target)?;
