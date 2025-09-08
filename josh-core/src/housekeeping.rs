f91135f4e49e12f733f99ce9a0c703357f121666
Christian Schilling <christian.schilling.de@gmail.com>
2025-09-08 17:00:53 +0200

    Fix workspace discovery (#1470)
    The workspace discovery logic looks for all
    workspace.josh files in the tree and populates
    the "known filters" list.
    However a workspace.josh file in the root of the
    tree results in a filter with invalid syntax, so it
    is skipped now.
    
    Change: fix-ws-discovery

--- josh-core/src/housekeeping.rs
+++ josh-core/src/housekeeping.rs
@@ -209,11 +209,12 @@ pub fn find_all_workspaces_and_subdirectories(
     let _trace_s = span!(Level::TRACE, "find_all_workspaces_and_subdirectories");
     let mut hs = std::collections::HashSet::new();
     tree.walk(git2::TreeWalkMode::PreOrder, |root, entry| {
-        if entry.name() == Some("workspace.josh") {
-            hs.insert(format!(":workspace={}", root.trim_matches('/')));
-        }
         if root.is_empty() {
             return 0;
+        }
+
+        if entry.name() == Some("workspace.josh") {
+            hs.insert(format!(":workspace={}", root.trim_matches('/')));
         }
         let v = format!("::{}/", root.trim_matches('/'));
         if v.chars().filter(|x| *x == '/').count() < 3 {
