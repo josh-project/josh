972463ccfd432faae6a4f9dd50f5765f37624f1e
Christian Schilling <christian.schilling.de@gmail.com>
2025-12-20 16:50:05 +0100

    Remove permissions filter support
    This was a half done, undocumented, feature that nobody used.

--- josh-core/src/housekeeping.rs
+++ josh-core/src/housekeeping.rs
@@ -279,12 +279,8 @@ pub fn refresh_known_filters(
                 &to_filtered_ref(upstream_repo, filter_spec),
                 upstream_repo,
             ) {
-                let (mut u, _) = filter_refs(
-                    transaction_overlay,
-                    filter::parse(filter_spec)?,
-                    &[from],
-                    filter::Filter::new().empty(),
-                );
+                let (mut u, _) =
+                    filter_refs(transaction_overlay, filter::parse(filter_spec)?, &[from]);
                 u[0].0 = to_ref;
                 updated_refs.append(&mut u);
             }
