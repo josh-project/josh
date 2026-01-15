1a602af8e11932aff3d3aa815253eb52d2affedf
Vladislav Ivanov <vlad@ivanov.email>
2026-01-15 21:54:44 +0100

    Stream git-http-backend CGI input and output

--- josh-proxy/src/http.rs
+++ josh-proxy/src/http.rs
@@ -1,7 +1,13 @@
 use axum::body::Body;
 use axum::http::{Response, StatusCode};
 use axum::response::IntoResponse;
+use futures::Stream;
+use pin_project_lite::pin_project;
+
 use josh_core::JoshError;
+
+use std::pin::Pin;
+use std::task::{Context, Poll};
 
 // Wrapper to make JoshError work with axum's IntoResponse
 // while not running into coherence rules
@@ -23,5 +29,44 @@ where
 {
     fn from(err: E) -> Self {
         ProxyError(err.into())
+    }
+}
+
+pin_project! {
+    pub struct StreamWithGuard<S, G> {
+        #[pin]
+        stream: S,
+        _guard: G,
+    }
+}
+
+impl<S, G> StreamWithGuard<S, G> {
+    pub fn new(stream: S, guard: G) -> Self {
+        Self {
+            stream,
+            _guard: guard,
+        }
+    }
+}
+
+impl<S: Stream, G> Stream for StreamWithGuard<S, G> {
+    type Item = S::Item;
+
+    fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        self.project().stream.poll_next(cx)
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        self.stream.size_hint()
+    }
+}
+
+impl<S, G> IntoResponse for StreamWithGuard<S, G>
+where
+    S: Stream<Item = Result<axum::body::Bytes, std::io::Error>> + Send + 'static,
+    G: Send + 'static,
+{
+    fn into_response(self) -> Response<Body> {
+        Body::from_stream(self).into_response()
     }
 }
