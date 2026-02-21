9e76bfb485960d7609d19cfc23203bd70622c47c
Vladislav Ivanov <vlad@ivanov.email>
2026-02-21 23:21:51 +0100

    Rework CLI auth organization
    * Move CLI-specific code (e.g. keyring stuff) back to CLI
    * Store JSON-serialized credential instead of separate credential
    records, to reduce keyring access OS confirmations in certain scenarios
    * On MacOS, keyring access without repeated password prompts is
    seemingly only possible when the binary is signed. We will eventually
    sign the CLI binary, but for now, store the token in a similar way other
    tools like gh store their tokens -- just in a file in user directory.
    This will be useful for development, and for releases we can set up
    proper codesign and safe storage.
    
    Change-Id: I6b5f3afbfd05326d3f99b2ede80c8fadf51f8125

---
+++ josh-cli/src/forge/mod.rs
@@ -0,0 +1,39 @@
+use clap::ValueEnum;
+use std::fmt::Formatter;
+
+pub mod github;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, ValueEnum)]
+pub enum Forge {
+    /// GitHub
+    Github,
+}
+
+impl std::fmt::Display for Forge {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Forge::Github => f.write_str("github"),
+        }
+    }
+}
+
+pub fn guess_forge(url: &str) -> Option<Forge> {
+    if josh_github_auth::is_github_url(url) {
+        return Some(Forge::Github);
+    }
+
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::forge::{Forge, guess_forge};
+
+    #[test]
+    fn test_guess_forge() {
+        assert_eq!(
+            guess_forge("https://github.com/josh-project/josh.git"),
+            Some(Forge::Github)
+        )
+    }
+}
--- josh-cli/src/lib.rs
+++ josh-cli/src/lib.rs
@@ -1,3 +1,4 @@
 pub mod commands;
 pub mod config;
 pub mod forge;
+pub mod keyring;
